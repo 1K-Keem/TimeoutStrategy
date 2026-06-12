0e1003614c9a85e3dc7c056cdec7b739ace27695
giakhanh <vogiakhanh212@gmail.com>
2026-06-12 11:31:42 +0700

    class WFGraph

--- include/DeadlockDetector.hpp
+++ include/DeadlockDetector.hpp
@@ -1,2 +1,25 @@
 // Todo: Khánh
+// hpp
 
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class WFGraph {
+private:
+  unordered_map<string, int> pidToIndex;
+  vector<string> indexToPid;
+  vector<unordered_set<int>> adj;
+
+  bool hasCycle();
+  bool dfs(int idx, vector<bool> &visited, vector<bool> &inStack);
+
+public:
+  int getPid(const string &pid);
+  void addEdge(const string &wait, const string &hold);
+  void removeEdge(const string &wait, const string &hold);
+  bool deadlockDetection();
+  bool precheck_deadlock();
+};
