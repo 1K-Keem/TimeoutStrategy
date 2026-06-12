187f0b5e25a7f00438282ac551fea93db4654d18
giakhanh <vogiakhanh212@gmail.com>
2026-06-12 16:45:23 +0700

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
-// TODO(Nhom): Noi CLI sau khi core engine, parser, detector va metrics san sang.
+// TODO(Nhom): Noi CLI sau khi core engine, parser, detector va metrics san
+// sang.
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
