06ae6cd48c50205fd5839dab08feb492977e0dbe
monoz2509k <nguyenkhoa24.6.2006@gmail.com>
2026-06-13 15:48:24 +0700

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Edu/OS/extends/TimeoutStrategy
-# Build directory: D:/Edu/OS/extends/TimeoutStrategy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(run_tests "D:/Edu/OS/extends/TimeoutStrategy/build/run_tests.exe")
-set_tests_properties(run_tests PROPERTIES  _BACKTRACE_TRIPLES "D:/Edu/OS/extends/TimeoutStrategy/CMakeLists.txt;33;add_test;D:/Edu/OS/extends/TimeoutStrategy/CMakeLists.txt;0;")
